86c3ed1ba8af5920706789f29e751d9dc2d2d863
joen0001 <joen0001@student.monash.edu>
2022-08-18 14:31:15 +1000

    Update piCam.py

--- piCam.py
+++ piCam.py
@@ -3,6 +3,14 @@ from time import sleep
 
 camera = PiCamera()
 camera.start_preview()
-sleep(5)
-camera.capture('/home/pi/TRC3000/images/image.jpg')
+
+i = 0
+try:
+    while True:
+        sleep(5)
+        name = '/home/pi/TRC3000/images/pic{}'.format(i)
+        camera.capture(name)
+        i += 1
+except KeyboardInterrupt:
+    print('Camera Stopped')
 camera.stop_preview()
