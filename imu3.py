90ae495645cb284bbaf994ad0cfedc045da8c1ba
joen0001 <joen0001@student.monash.edu>
2022-08-25 12:45:59 +1000

    update

--- imu3.py
+++ imu3.py
@@ -1,5 +1,5 @@
 from mpu6050 import mpu6050
-import time
+import time, math
 mpu = mpu6050(0x68)
 
 while True:
@@ -7,9 +7,9 @@ while True:
     print()
 
     accel_data = mpu.get_accel_data()
-    print("Acceleration: X:"+str(accel_data['x'])+ ", Y:"+str(accel_data['y']) + ", Z:" +str(accel_data['z']) )
+    print("Acceleration: X:"+str(math.floor(accel_data['x']))+ ", Y:"+str(math.floor(accel_data['y'])) + ", Z:" +str(math.floor(accel_data['z']+12)))
 
     gyro_data = mpu.get_gyro_data()
-    print("Gyroscope: X:"+str(gyro_data['x'])+ ", Y:"+str(gyro_data['y']) + ", Z:" +str(gyro_data['z']))
+    print("Gyroscope: X:"+str(math.floor(gyro_data['x']))+ ", Y:"+str(math.floor(gyro_data['y'])) + ", Z:" +str(math.floor(gyro_data['z'])))
     print("-------------------------------")
     time.sleep(1)
