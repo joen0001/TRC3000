f6d7e3b790c8b07553626b01512ac7caa0a85bf8
joen0001 <joen0001@student.monash.edu>
2022-08-25 14:41:00 +1000

    Update main.py

--- main.py
+++ main.py
@@ -63,6 +63,8 @@ def PrintIMU():
 def CheckIMU():
     gyro_data = mpu.get_gyro_data()
     accel_data = mpu.get_gyro_data()
+    #if accel_data['x'] or accel_data['y'] or accel_data['z'] >= 10000:
+    #    return 1
     if accel_data['x'] or accel_data['y'] or accel_data['z'] >= 10000:
         return 1
     if gyro_data['x'] or gyro_data['y'] or gyro_data['z'] >= 10000:
