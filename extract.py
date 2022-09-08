0460ed8434e308910a5fb0d79824fc85ac19fd9d
joen0001 <joen0001@student.monash.edu>
2022-09-08 13:52:21 +1000

    Update extract.py

--- extract.py
+++ extract.py
@@ -4,6 +4,6 @@ im = Image.open('images/pic0.jpg') # Can be many different formats.
 pix = im.load()
 x,y = im.size  # Get the width and hight of the image for iterating over
 print (x, y)  # Get the RGBA Value of the a pixel of an image
-pix[100:200,100:200] = (255,255,255,255)
+pix[5] = (255,255,255,255)
 # pix[x,y] = value  # Set the RGBA Value of the image (tuple)
 im.save('images/adj_pic0.jpg')
