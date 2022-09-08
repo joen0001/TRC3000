ccc22a6c424e696f69dce2324c87acb92427d21d
joen0001 <joen0001@student.monash.edu>
2022-09-08 14:04:13 +1000

    Update extract.py

--- extract.py
+++ extract.py
@@ -3,7 +3,7 @@ from PIL import Image
 im = Image.open('images/pic0.jpg') # Can be many different formats.
 pix = im.load()
 width,height = im.size  # Get the width and hight of the image for iterating over
-area = (width/4,height/4,width/4,height/4)
+area = (width/4,height/4,3*width/4,3*height/4)
 im = im.crop(area)
 # pix[x,y] = value  # Set the RGBA Value of the image (tuple)
 im.save('images/adj_pic0.jpg')
