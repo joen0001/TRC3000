80be814e5f3cb5304e4a61873e7f2b7b8f5194f6
joen0001 <joen0001@student.monash.edu>
2022-09-15 12:52:40 +1000

    Update extract2.py

--- extract2.py
+++ extract2.py
@@ -15,10 +15,10 @@ crop = im.crop(area)
 width,height = crop.size
 n = width*height
 pix = crop.load()
-print(pix[0,0])
-R = np.mean(pix[:,0])
-G = np.mean(pix[:,1])
-B = np.mean(pix[:,2])
+print(pix[0,0,0])
+R = np.mean(pix[:,:,0])
+G = np.mean(pix[:,:,1])
+B = np.mean(pix[:,:,2])
 
 R_avg = round(R/n)
 G_avg = round(G/n)
