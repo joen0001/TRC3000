dfa520aa5c7b9b6303e45b7fa57af196ec45d557
joen0001 <joen0001@student.monash.edu>
2022-09-15 14:03:28 +1000

    Update extract2.py

--- extract2.py
+++ extract2.py
@@ -1,45 +1,49 @@
 from PIL import Image, ImageDraw
 import numpy as np
 
-R = 0
-G = 0
-B = 0
-BOX_SIZE = 100
+def extract(image_path, BOX_SIZE=100):
+    R = 0
+    G = 0
+    B = 0
 
-im = Image.open('images/pic0.jpg') # Can be many different formats.
-width,height = im.size  # Get the width and hight of the image for iterating over
+    im = Image.open(image_path) # Can be many different formats.
+    width,height = im.size  # Get the width and hight of the image for iterating over
 
-area = (width/2-BOX_SIZE,height/2-BOX_SIZE,width/2+BOX_SIZE,height/2+BOX_SIZE)
-crop = im.crop(area)
+    area = (width/2-BOX_SIZE,height/2-BOX_SIZE,width/2+BOX_SIZE,height/2+BOX_SIZE)
+    crop = im.crop(area)
 
-width_crop,height_crop = crop.size
-n = width_crop*width_crop
-pix = crop.load()
+    width_crop,height_crop = crop.size
+    n = width_crop*width_crop
+    pix = crop.load()
 
-for i in range(width_crop):
-    for j in range(height_crop):
-        pixel = pix[i,j]
-        R += pixel[0]
-        G += pixel[1]
-        B += pixel[2]
+    for i in range(width_crop):
+        for j in range(height_crop):
+            pixel = pix[i,j]
+            R += pixel[0]
+            G += pixel[1]
+            B += pixel[2]
 
-R_avg = round(R/n)
-G_avg = round(G/n)
-B_avg = round(B/n)
+    R = round(R/n)
+    G = round(G/n)
+    B = round(B/n)
 
-print(R_avg,B_avg,G_avg)
-draw = ImageDraw.Draw(im)
-draw.rectangle([width/2-BOX_SIZE,height/2-BOX_SIZE,width/2+BOX_SIZE,height/2+BOX_SIZE], outline=(R_avg,B_avg,G_avg), width=10)
+    draw = ImageDraw.Draw(im)
+    draw.rectangle([width/2-BOX_SIZE,height/2-BOX_SIZE,width/2+BOX_SIZE,height/2+BOX_SIZE], outline=(R,G,B), width=10)
+
+    if R > G + B:
+        colour = 'Red'
+    elif G > R + B:
+        colour = 'Green'  
+    elif B > R + G:
+        colour = 'Blue'
+    else:
+        colour = 'Clear/White'
+
+    draw.text([width/2-BOX_SIZE,height/2+BOX_SIZE+20], colour)
+
+    path, _ = image_path.split(".")
+    new_name = path + 'proc.jpg'
+    im.save(new_name)
 
 
-
-im.save('images/adj_pic0.jpg')
-
-# Bounding box for specific area
-# Average Pixels to give basic color
-# Dict for color values
-
-# FOAMING
-# Vertical pixel array
-# Detect colour vs foam
-# Foam height
+extract('pic0.jpg', BOX_SIZE=100)
