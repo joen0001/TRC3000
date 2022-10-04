a25d328f6eff23bee2e09ca09d1e95643f9f5ef3
joen0001 <joen0001@student.monash.edu>
2022-10-04 17:14:10 +1100

    Update foam.py

--- foam.py
+++ foam.py
@@ -4,19 +4,13 @@ import numpy as np
 def foam(image_path,GRID_BOX_SIZE=20, THRESHOLD = 40):
     im = Image.open(image_path) # Can be many different formats.
     width,height = im.size  # Get the width and hight of the image for iterating over
-
-    area = (width/2-GRID_BOX_SIZE/2,0,width/2+GRID_BOX_SIZE/2,height)
-    crop = im.crop(area)
-
-    width_crop,height_crop = crop.size
-    n = width_crop*height_crop
-    pix = crop.load()
+    pix = im.load()
 
     grid_avg = 0
     mat_avg = []
     change = []
     current_avg = 0-THRESHOLD
-    for i in range(height_crop):
+    for i in range(height):
         for j in range(GRID_BOX_SIZE):
             pixel = pix[j,i]
             grid_avg += sum(pixel)
@@ -29,14 +23,13 @@ def foam(image_path,GRID_BOX_SIZE=20, THRESHOLD = 40):
             grid_avg = 0
     print(change)
 
-
     draw = ImageDraw.Draw(im)
 
     height_foam = '2'
     draw.text([0,0], text=height_foam, fill=(0,0,0))
-    draw.line([width/2,0,width/2,height],fill=(0,0,0),width=5)
+    draw.line([width/2,0,width/2,height],fill=(0,0,0))
     for x in change:
-        draw.line([0,x,width,x],fill=(0,0,0))
+        draw.line([0,x,width,x],fill=(0,0,0),width=5)
 
     path, _ = image_path.split(".")
     new_name = path + 'proc.jpg'
