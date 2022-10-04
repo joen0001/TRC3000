e566ec0d9c41012248ad951fddb2f8addf600a1c
joen0001 <joen0001@student.monash.edu>
2022-10-04 17:38:18 +1100

    Update foam.py

--- foam.py
+++ foam.py
@@ -1,7 +1,7 @@
 from PIL import Image, ImageDraw, ImageFont
 import numpy as np
 
-def foam(image_path,GRID_BOX_SIZE=10, THRESHOLD = 10):
+def foam(image_path,GRID_BOX_SIZE=10, THRESHOLD = 30):
     im = Image.open(image_path) # Can be many different formats.
     width,height = im.size  # Get the width and hight of the image for iterating over
     pix = im.load()
