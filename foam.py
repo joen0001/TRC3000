df0c019dfbfc7a0e36f3884f42e1cac818b6a03c
joen0001 <joen0001@student.monash.edu>
2022-10-04 16:07:03 +1100

    Update foam.py

--- foam.py
+++ foam.py
@@ -1,7 +1,7 @@
 from PIL import Image, ImageDraw, ImageFont
 import numpy as np
 
-def foam(image_path, GRID_BOX_SIZE=50, THRESHOLD = 50):
+def foam(image_path, GRID_BOX_SIZE=20, THRESHOLD = 50):
     im = Image.open(image_path) # Can be many different formats.
     width,height = im.size  # Get the width and hight of the image for iterating over
 
@@ -9,17 +9,21 @@ def foam(image_path, GRID_BOX_SIZE=50, THRESHOLD = 50):
     crop = im.crop(area)
 
     draw = ImageDraw.Draw(im)
-    #draw.rectangle(area, outline=(R,G,B), width=10)
+    draw.rectangle(area, outline=(0,0,0), width=10)
 
     width_crop,height_crop = crop.size
     n = width_crop*height_crop
     pix = crop.load()
 
-    for i in range(0,height_crop,GRID_BOX_SIZE):
-        for j in range(0,height_crop,GRID_BOX_SIZE):
-            print(pix[0:2,0])
-            grid_sum = sum(pix[i:GRID_BOX_SIZE,j:GRID_BOX_SIZE])
-        
+    grid_ind = 0
+    while grid_ind < height//GRID_BOX_SIZE:
+        for i in range(GRID_BOX_SIZE*grid_ind,GRID_BOX_SIZE*((grid_ind+1)),1): # Height
+            for j in range(GRID_BOX_SIZE):
+                # Count grid x grid size pixel box and average
+                pixel = pix[i,j]
+                val += sum(pixel)
+                
+    print(val)
 
     height_foam = 2
     draw.text([0,0], text=height_foam, fill=(0,0,0))
