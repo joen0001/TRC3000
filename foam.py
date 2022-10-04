06de738c3bcabfccf0fedd3422c4aee968eca56b
joen0001 <joen0001@student.monash.edu>
2022-10-04 17:32:50 +1100

    Update foam.py

--- foam.py
+++ foam.py
@@ -1,12 +1,11 @@
 from PIL import Image, ImageDraw, ImageFont
 import numpy as np
 
-def foam(image_path,GRID_BOX_SIZE=20, THRESHOLD = 15):
+def foam(image_path,GRID_BOX_SIZE=15, THRESHOLD = 15):
     im = Image.open(image_path) # Can be many different formats.
     width,height = im.size  # Get the width and hight of the image for iterating over
     pix = im.load()
 
-    grid_avg = 0
     mat_avg = []
     change = []
     current_avg = 0-THRESHOLD
@@ -21,7 +20,7 @@ def foam(image_path,GRID_BOX_SIZE=20, THRESHOLD = 15):
                 change.append(i)  
                 current_avg = grid_avg
             grid_avg = 0
-    print(mat_avg)
+        
     print(change)
 
     draw = ImageDraw.Draw(im)
