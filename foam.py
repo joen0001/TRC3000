1a0160bcddff44b4db6d9802b9016ac234e466ea
joen0001 <joen0001@student.monash.edu>
2022-10-04 17:24:25 +1100

    Update foam.py

--- foam.py
+++ foam.py
@@ -1,7 +1,7 @@
 from PIL import Image, ImageDraw, ImageFont
 import numpy as np
 
-def foam(image_path,GRID_BOX_SIZE=20, THRESHOLD = 5):
+def foam(image_path,GRID_BOX_SIZE=20, THRESHOLD = 10):
     im = Image.open(image_path) # Can be many different formats.
     width,height = im.size  # Get the width and hight of the image for iterating over
     pix = im.load()
@@ -29,8 +29,8 @@ def foam(image_path,GRID_BOX_SIZE=20, THRESHOLD = 5):
     height_foam = '2'
     draw.text([0,0], text=height_foam, fill=(0,0,0))
     draw.line([width/2,0,width/2,height],fill=(0,0,0))
-    for x in change[1:-1]:
-        draw.line([0,x,width,x],fill=(0,0,0),width=5)
+    for x in change[2:-1]:
+        draw.line([0,x,width,x],fill=(0,0,0),width=10)
 
     path, _ = image_path.split(".")
     new_name = path + 'proc.jpg'
