28d5243b590aea0adf17b1e1d12bcd1080272919
joen0001 <joen0001@student.monash.edu>
2022-10-04 15:56:44 +1100

    Update foam.py

--- foam.py
+++ foam.py
@@ -1,27 +1,24 @@
 from PIL import Image, ImageDraw, ImageFont
 import numpy as np
 
-def foam(image_path, GRID_BOX_SIZE=50):
+def foam(image_path, GRID_BOX_SIZE=50, THRESHOLD = 50):
     im = Image.open(image_path) # Can be many different formats.
     width,height = im.size  # Get the width and hight of the image for iterating over
 
-    area = (width/2-GRID_BOX_SIZE,0,width/2+GRID_BOX_SIZE,height)
+    area = (width/2-GRID_BOX_SIZE/2,0,width/2+GRID_BOX_SIZE/2,height)
     crop = im.crop(area)
 
     draw = ImageDraw.Draw(im)
-    draw.rectangle(area, outline=(R,G,B), width=10)
+    #draw.rectangle(area, outline=(R,G,B), width=10)
 
     width_crop,height_crop = crop.size
-    n = width_crop*width_crop
+    n = width_crop*height_crop
     pix = crop.load()
 
-    for j in range(0,height_crop,10):
-        pixelset10 = pix[width_crop/2,j:j+10]
-        for p in range(j):
-            pixel = pix[width_crop/2,]
-            R += pixel[0]
-            G += pixel[1]
-            B += pixel[2]
+    for i in range(0,height_crop,GRID_BOX_SIZE):
+        for j in range(0,height_crop,GRID_BOX_SIZE):
+            print(pix[i:GRID_BOX_SIZE,j:GRID_BOX_SIZE])
+            grid_sum = sum(pix[i:GRID_BOX_SIZE,j:GRID_BOX_SIZE])
         
 
     height_foam = 2
