94a80c7ad0b715fdf77ede1624bdc13870fc14d5
joen0001 <joen0001@student.monash.edu>
2022-10-04 17:52:45 +1100

    Update foam.py

--- foam.py
+++ foam.py
@@ -17,9 +17,8 @@ def foam(image_path,GRID_BOX_SIZE=10, THRESHOLD = 30):
             grid_avg = round(grid_avg/(GRID_BOX_SIZE*3))
             mat_avg.append(grid_avg)
             if current_avg > 200 and abs(current_avg-grid_avg) > THRESHOLD:
-                print(current_avg)
                 change.append(i)  
-                current_avg = grid_avg
+            current_avg = grid_avg
             grid_avg = 0
         
     print(change)
