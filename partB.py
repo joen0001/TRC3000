422c6d660e9055a8302f5cd53d9812db4a3de66b
joen0001 <joen0001@student.monash.edu>
2022-10-06 14:58:45 +1100

    Update partB.py

--- partB.py
+++ partB.py
@@ -118,72 +118,75 @@ def read_raw_data(addr):
         return value
 
 def IMU_Reading():
-    accX = read_raw_data(ACCEL_XOUT_H)
-    accY = read_raw_data(ACCEL_YOUT_H)
-    accZ = read_raw_data(ACCEL_ZOUT_H)
+    try:
+        accX = read_raw_data(ACCEL_XOUT_H)
+        accY = read_raw_data(ACCEL_YOUT_H)
+        accZ = read_raw_data(ACCEL_ZOUT_H)
 
-    #Read Gyroscope raw value
-    gyroX = read_raw_data(GYRO_XOUT_H)
-    gyroY = read_raw_data(GYRO_YOUT_H)
-    gyroZ = read_raw_data(GYRO_ZOUT_H)
-    print('0')
-    dt = time.time() - timer
-    timer = time.time()
-    print('1')
-    if (RestrictPitch):
-        roll = math.atan2(accY,accZ) * radToDeg
-        pitch = math.atan(-accX/math.sqrt((accY**2)+(accZ**2))) * radToDeg
-    else:
-        roll = math.atan(accY/math.sqrt((accX**2)+(accZ**2))) * radToDeg
-        pitch = math.atan2(-accX,accZ) * radToDeg
-    print('2')
-    gyroXRate = gyroX/131
-    gyroYRate = gyroY/131
-
-    print('3')
-    if (RestrictPitch):
-
-        if((roll < -90 and kalAngleX >90) or (roll > 90 and kalAngleX < -90)):
-            kalmanX.setAngle(roll)
-            complAngleX = roll
-            kalAngleX   = roll
-            gyroXAngle  = roll
+	    #Read Gyroscope raw value
+        gyroX = read_raw_data(GYRO_XOUT_H)
+        gyroY = read_raw_data(GYRO_YOUT_H)
+        gyroZ = read_raw_data(GYRO_ZOUT_H)
+        print('0')
+        dt = time.time() - timer
+        timer = time.time()
+        print('1')
+        if (RestrictPitch):
+            roll = math.atan2(accY,accZ) * radToDeg
+            pitch = math.atan(-accX/math.sqrt((accY**2)+(accZ**2))) * radToDeg
         else:
-            kalAngleX = kalmanX.getAngle(roll,gyroXRate,dt)
+            roll = math.atan(accY/math.sqrt((accX**2)+(accZ**2))) * radToDeg
+            pitch = math.atan2(-accX,accZ) * radToDeg
+        print('2')
+        gyroXRate = gyroX/131
+        gyroYRate = gyroY/131
 
-        if(abs(kalAngleX)>90):
-            gyroYRate  = -gyroYRate
-            kalAngleY  = kalmanY.getAngle(pitch,gyroYRate,dt)
-    else:
-        if((pitch < -90 and kalAngleY >90) or (pitch > 90 and kalAngleY < -90)):
-            kalmanY.setAngle(pitch)
-            complAngleY = pitch
-            kalAngleY   = pitch
-            gyroYAngle  = pitch
+        print('3')
+        if (RestrictPitch):
+
+            if((roll < -90 and kalAngleX >90) or (roll > 90 and kalAngleX < -90)):
+                kalmanX.setAngle(roll)
+                complAngleX = roll
+                kalAngleX   = roll
+                gyroXAngle  = roll
+            else:
+                kalAngleX = kalmanX.getAngle(roll,gyroXRate,dt)
+
+            if(abs(kalAngleX)>90):
+                gyroYRate  = -gyroYRate
+                kalAngleY  = kalmanY.getAngle(pitch,gyroYRate,dt)
         else:
-            kalAngleY = kalmanY.getAngle(pitch,gyroYRate,dt)
+            if((pitch < -90 and kalAngleY >90) or (pitch > 90 and kalAngleY < -90)):
+                kalmanY.setAngle(pitch)
+                complAngleY = pitch
+                kalAngleY   = pitch
+                gyroYAngle  = pitch
+            else:
+                kalAngleY = kalmanY.getAngle(pitch,gyroYRate,dt)
 
-        if(abs(kalAngleY)>90):
-            gyroXRate  = -gyroXRate
-            kalAngleX = kalmanX.getAngle(roll,gyroXRate,dt)
-    print('4')
-    #angle = (rate of change of angle) * change in time
-    gyroXAngle = gyroXRate * dt
-    gyroYAngle = gyroYAngle * dt
+            if(abs(kalAngleY)>90):
+                gyroXRate  = -gyroXRate
+                kalAngleX = kalmanX.getAngle(roll,gyroXRate,dt)
+        print('4')
+		#angle = (rate of change of angle) * change in time
+        gyroXAngle = gyroXRate * dt
+        gyroYAngle = gyroYAngle * dt
 
-    #compAngle = constant * (old_compAngle + angle_obtained_from_gyro) + constant * angle_obtained from accelerometer
-    compAngleX = 0.93 * (compAngleX + gyroXRate * dt) + 0.07 * roll
-    compAngleY = 0.93 * (compAngleY + gyroYRate * dt) + 0.07 * pitch
+		#compAngle = constant * (old_compAngle + angle_obtained_from_gyro) + constant * angle_obtained from accelerometer
+        compAngleX = 0.93 * (compAngleX + gyroXRate * dt) + 0.07 * roll
+        compAngleY = 0.93 * (compAngleY + gyroYRate * dt) + 0.07 * pitch
 
-    if ((gyroXAngle < -180) or (gyroXAngle > 180)):
-        gyroXAngle = kalAngleX
-    if ((gyroYAngle < -180) or (gyroYAngle > 180)):
-        gyroYAngle = kalAngleY
-    print('5')
-    A_x = accX/16384.0
-    A_y = accX/16384.0
-    A_z = accX/16384.0
-    return(kalAngleX-180,kalAngleY,gyroZ,A_x,A_y,A_z)
+        if ((gyroXAngle < -180) or (gyroXAngle > 180)):
+            gyroXAngle = kalAngleX
+        if ((gyroYAngle < -180) or (gyroYAngle > 180)):
+            gyroYAngle = kalAngleY
+        print('5')
+        A_x = accX/16384.0
+        A_y = accX/16384.0
+        A_z = accX/16384.0
+        return(kalAngleX-180,kalAngleY,gyroZ,A_x,A_y,A_z)
+    except Exception as exc:
+        flag += 1
 
 def CameraCapture(image):
     name = "/home/pi/TRC3000/images/pic"+str(image)+".jpg"
