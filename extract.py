e60d1974a5116452ec61877144ebbde0bdb505de
joen0001 <61935138+joen0001@users.noreply.github.com>
2022-10-06 12:42:14 +1100

    Fix

--- extract.py
+++ extract.py
@@ -1,7 +1,7 @@
 from PIL import Image, ImageDraw, ImageFont
 import numpy as np
 
-def extract(image_path, BOX_SIZE=80,GRID_BOX_SIZE=10,THRESHOLD = 30):
+def extract(image_path, BOX_SIZE=80,GRID_BOX_SIZE=10,THRESHOLD = 25):
     BOX_SIZE = BOX_SIZE/2
     R = 0
     G = 0
@@ -58,10 +58,10 @@ def extract(image_path, BOX_SIZE=80,GRID_BOX_SIZE=10,THRESHOLD = 30):
     # DRAWING/OUTPUT
     draw = ImageDraw.Draw(im)
     draw.rectangle([width/2-BOX_SIZE,height/2-BOX_SIZE,width/2+BOX_SIZE,height/2+BOX_SIZE], outline=(R,G,B), width=4)
-    draw.text([0,0], text=colour, fill=(R,G,B))
     draw.line([width/2,0,width/2,height],fill=(0,0,0))
     for x in change:
-        draw.line([0,x,width,x],fill=(0,0,0),width=10)
+        draw.line([0,x,width,x],fill=(0,0,0),width=4)
+    draw.text([0,0], text=colour, fill=(R,G,B))
 
     path, _ = image_path.split(".")
     new_name = path + 'proc.jpg'
