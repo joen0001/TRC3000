7f61e44f3a656061b65ccefc3d75662b39f871c7
joen0001 <61935138+joen0001@users.noreply.github.com>
2022-10-12 14:31:48 +1100

    Commenting

--- extract.py
+++ extract.py
@@ -7,17 +7,22 @@ def extract(image_path, BOX_SIZE=80,GRID_BOX_SIZE=10,THRESHOLD = 25):
     G = 0
     B = 0
 
-    im = Image.open(image_path) # Can be many different formats.
-    width,height = im.size  # Get the width and hight of the image for iterating over
+    # Open image from file path
+    im = Image.open(image_path)
+    # Get the width and hight of the image
+    width,height = im.size 
 
+    # Create area within image to check color
     area = (width/2-BOX_SIZE,height/2-BOX_SIZE,width/2+BOX_SIZE,height/2+BOX_SIZE)
+    # New cropped version of image
     crop = im.crop(area)
     
-
+    # New dimensions of cropped area
     width_crop,height_crop = crop.size
     n = width_crop*height_crop
     pix = crop.load()
 
+    # Adding the individual R,G,B values
     for i in range(width_crop):
         for j in range(height_crop):
             pixel = pix[i,j]
@@ -25,32 +30,34 @@ def extract(image_path, BOX_SIZE=80,GRID_BOX_SIZE=10,THRESHOLD = 25):
             G += pixel[1]
             B += pixel[2]
 
+    # Average individual channel values
     R = round(R/n)
     G = round(G/n)
     B = round(B/n)
     colour = [R,G,B]
-
-    #if R > G + B:
-    #    colour = 'Red: ' + str([R,G,B])
-    #elif G > R + B:
-    #    colour = 'Green: ' + str([R,G,B])
-    #elif B > R + G:
-    #    colour = 'Blue: ' + str([R,G,B])
-    #else:
-    #    colour = 'Clear/White + ' + str([R,G,B])
 
     # FOAMING
     pix = im.load()
     grid_avg = 0
     mat_avg = []
     change = []
+    
+    # Threshold to determine if there is a different layer
     current_avg = 0-THRESHOLD
+    
+    # Iterating over height
     for i in range(height):
+        # Summing the values of grid_box_size in a column to be averaged
         pixel = pix[width/2,i]
         grid_avg += sum(pixel)
+        
         if i % GRID_BOX_SIZE == 0:
+            # Taking the average pixel values of each section
             grid_avg = round(grid_avg/(GRID_BOX_SIZE*3))
             mat_avg.append(grid_avg)
+            
+            # If the difference in average pixel values per section is greater than threshold,
+            # Count changes as a different section
             if abs(current_avg-grid_avg) > THRESHOLD:
                 change.append(i)  
                 current_avg = grid_avg
@@ -58,17 +65,24 @@ def extract(image_path, BOX_SIZE=80,GRID_BOX_SIZE=10,THRESHOLD = 25):
     
     # DRAWING/OUTPUT
     draw = ImageDraw.Draw(im)
+    # Draw bounding box used for color analysis
     draw.rectangle([width/2-BOX_SIZE,height/2-BOX_SIZE,width/2+BOX_SIZE,height/2+BOX_SIZE], outline=(0,0,0), width=4)
-    #draw.line([width/2,0,width/2,height],fill=(0,0,0))
-    #for x in change:
-    #    draw.line([0,x,width,x],fill=(0,0,0),width=4)
+    
+    # Output Specific RGB Colour values + Background box
     draw.rectangle([0,0,200,15], (255,255,255), width=-1)
     draw.text([0,0], text=str(colour), fill=(R,G,B))
-
+    
+    # Draw Lines at each new section
+    draw.line([width/2,0,width/2,height],fill=(0,0,0))
+    for x in change:
+        draw.line([0,x,width,x],fill=(0,0,0),width=4)
+    
+    # Save processed image under new name
     path, _ = image_path.split(".")
     new_name = path + 'proc.jpg'
     im.save(new_name)
     return colour
 
+# Run for these two pictures
 extract('images/pic0.jpg')
 extract('images/pic1.jpg')
